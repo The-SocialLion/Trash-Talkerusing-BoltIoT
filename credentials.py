4ae2330d11d73c9f26ee7e3714fb35618c9a6009
Vishal Balaji Sivaraman <64977199+The-SocialLion@users.noreply.github.com>
2020-10-05 08:54:55 +0530

    included credentials

---
+++ credentials.py
@@ -0,0 +1,17 @@
+# this is a python file which has all the credentials for bolt to access the cloud as well as access your twilio account via API
+
+ API = "bolt cloud api" 
+
+ ID  = "device" 
+
+ # Credentials required to send SMS 
+
+SID = 'your twillio account sid' 
+
+AUTH = 'your twillio account auth token' 
+
+FROM = 'your twillio number' 
+
+TO =' your number to get alert sms, please dont forget to add your country  code in the beginning of your phone number '
+
+# Save it in a separate file called as bolt.py and then we can acces this file in the main code by importing it there
